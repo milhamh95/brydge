5fa0bca0890dd8fe7db38a6edb9d53e2765b8d4e
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2021-11-07 09:48:41 +0700

    ref: simplify req and response

--- internal/app/inquiry/handler/handler.go
+++ internal/app/inquiry/handler/handler.go
@@ -3,49 +3,16 @@ package handler
 import "context"
 
 type request struct {
-	TrxId          string `json:"trxId"`
-	Origin         string `json:"origin"`
-	CustomerNumber string `json:"customerNumber"`
-	Msisdn         string `json:"msisdn"`
-	OrderCategory  string `json:"orderCategory"`
-	Destination    string `json:"destination"`
-	PaymentAmount  int    `json:"paymentAmount"`
-	TrxDate        string `json:"trxDate"`
-	Currency       string `json:"currency"`
-	Merchant       string `json:"merchant"`
-	Terminal       string `json:"terminal"`
-	MerchantUsr    string `json:"merchantUsr"`
-	MerchantPwd    string `json:"merchantPwd"`
-	Topic          string `json:"topic"` // general, specific, mfs
-	Product        string `json:"product"`
-	Shortcode      string `json:"shortcode"`
-	DenomId        string `json:"denomId"`
-	DenomName      string `json:"denomName,omitempty"`
-	DenomCode      string `json:"denomCode,omitempty"`
-	DenomAmount    int    `json:"denomAmount,omitempty"`
-
-	InquiryUrl string      `json:"inquiryUrl"`
-	ExtValue   []extraData `json:"extraData"`
+	TrxId         string `json:"trxId"`
+	ProductCode   string `json:"productCode"`
+	PaymentAmount int    `json:"paymentAmount"`
 }
 
 type response struct {
-	TrxId                string      `json:"trxId"`
-	Origin               string      `json:"origin"`
-	Destination          string      `json:"destination"`
-	DestinationAddress   string      `json:"destinationAddress,omitempty"`
-	DestinationData      string      `json:"destinationData,omitempty"`
-	DestinationReference string      `json:"destinationReference"`
-	PaymentAmount        int         `json:"paymentAmount"`
-	DenomAmount          interface{} `json:"denomAmount,omitempty"`
-
-	ExtraData []extraData `json:"extraData,omitempty"`
-}
-
-// ExtraData is an extra data for smartbiller
-type extraData struct {
-	Key    string `json:"key"`
-	Value  string `json:"value"`
-	IsCopy bool   `json:"isCopy,omitempty"`
+	ID            string `json:"id"`
+	PartnerID     string `json:"partnerId"`
+	ProductCode   string `json:"productCode"`
+	PaymentAmount int    `json:"paymentAmount"`
 }
 
 type useCase interface {
